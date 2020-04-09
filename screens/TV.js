9ac2a40629649467c25f0c9b63495a25db925a8d
idev <insert@kakao.com>
2020-04-09 19:39:28 +0900

    TV API connect and loaded

--- screens/TV.js
+++ screens/TV.js
@@ -1,10 +1,49 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import {View, Text} from 'react-native';
+import {tvAPI} from '../Api';
 
-export default class TV extends Component {
-  render() {
-    return (
-      <Text> TV </Text>
-    );
+export default ({navigation}) => {
+
+
+  const [tv, setTV] = useState({
+    today : [],
+    thisWeek : [],
+    topRated : [],
+    popular : [],
+    todayError : null,
+    thisWeekError : null,
+    topRatedError : null,
+    popularError : null
+  })
+
+  const getData = async () => {
+    const [today, todayError] = await tvAPI.today();
+    const [thisWeek, thisWeekError] = await tvAPI.thisWeek();
+    const [topRated, topRatedError] = await tvAPI.topRated();
+    const [popular, popularError] = await tvAPI.popular();
+
+    setTV({
+      today,
+      todayError,
+      thisWeek,
+      thisWeekError,
+      topRated,
+      topRatedError,
+      popular,
+      popularError
+    })
   }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return (
+    <View>
+      <Text>{tv.today?.length}</Text>
+    </View>
+  )
+
+
 }
+
