48fa61f4f7705e18b85027dc8db260649ceb9652
idev <insert@kakao.com>
2020-04-09 19:16:41 +0900

    create movie api

--- Api.js
+++ Api.js
@@ -8,3 +8,20 @@ const makeRequest = (path, params) =>
             api_key : TMDB_KEY
         }
     });
+
+export const movieAPI = {
+    nowPlaying : () => makeRequest('/movie/now_playing'),
+    popular : () => makeRequest('/movie/popular'),
+    upcoming : () => makeRequest('/movie/upcoming', {region : 'kr'}),
+    search : query => makeRequest('/search/movie', {query}),
+    movie : id => makeRequest(`/movie/${id}`)
+};
+
+export const tvAPI = {
+    today : () => makeRequest('/tv/airing_today'),
+    thisWeek : () => makeRequest('/tv/on_the_air'),
+    topRated : () => makeRequest('/tv/top_rated'),
+    popular : () => makeRequest('/tv/popular'),
+    search : query => makeRequest('/search/tv', {query}),
+    show : id => makeRequest(`/tv/${id}`)
+}
